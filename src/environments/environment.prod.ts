b350411c60ce3ef6cd9ad90f1f9f04dc8a684f8c
el shadai <imanuelsha@gmail.com>
2025-07-21 23:50:50 +0530

    Initial Angular frontend push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://backendforautobot-production.up.railway.app/auth'
+  apiBaseUrl: 'https://backendforautobot-production.up.railway.app'
 };
