b3f3b1ca37c6fe3ec7dba569394b18e129a2f11c
Johnwickdev <126758225+Johnwickdev@users.noreply.github.com>
2025-08-19 16:41:27 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // API base URL now points to the AWS Elastic Beanstalk deployment
-  apiBaseUrl: 'https://autotradxxebbu-env.eba-vp9dhc3a.eu-north-1.elasticbeanstalk.com'
-};
+  apiBaseUrl: 'autotradebot-env.eba-adnie9a5.eu-north-1.elasticbeanstalk.com'
+};            
